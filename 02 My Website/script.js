82af3aa39229216e8c77214f52488009102e3739
Kalfin Syah Kilau Mayya <110911274+KalfinSyah@users.noreply.github.com>
2023-01-24 20:58:10 +0700

    Delete 02 My Website directory

--- 02 My Website/script.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('nav');
-const hamburger = nav.firstElementChild.firstElementChild;
-
-hamburger.addEventListener('click', function(){
-    nav.classList.toggle('max-md:overflow-visible');
-    nav.classList.toggle('max-md:h-fit');
-
-});
